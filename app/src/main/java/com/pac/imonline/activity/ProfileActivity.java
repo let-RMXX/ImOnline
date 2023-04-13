4702c986ba76022fcf11785c5f27d0feb1b465c4
carlosalvaro98 <c.alvaro@outlook.pt>
2023-04-13 13:09:39 +0100

    Profile Activity
    Trying layouts for profile activity

---
+++ app/src/main/java/com/pac/imonline/activity/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.pac.imonline.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pac.imonline.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
