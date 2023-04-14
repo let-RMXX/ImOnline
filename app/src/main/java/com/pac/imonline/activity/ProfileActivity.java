7fb75d47ad30576f8da27755eb90d22682bcb668
carlosalvaro98 <c.alvaro@outlook.pt>
2023-04-14 10:40:47 +0100

    Profile/EditProfile Unfinished
    Button "edit profile" is now working.
    Still unfinished

--- app/src/main/java/com/pac/imonline/activity/ProfileActivity.java
+++ app/src/main/java/com/pac/imonline/activity/ProfileActivity.java
@@ -2,16 +2,32 @@ package com.pac.imonline.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.pac.imonline.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
-
+    private Button button_editprofile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        button_editprofile = (Button) findViewById(R.id.button_editprofile);
+        button_editprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditProfileActivity();
+            }
+        });
+    }
+
+    public void openEditProfileActivity(){
+        Intent editprofile = new Intent(this, EditProfileActivity.class);
+        startActivity(editprofile);
     }
 }
