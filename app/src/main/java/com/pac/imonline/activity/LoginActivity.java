c0ed84ab3910be41666da5933b42e065573a2a52
Ana Lopes <24286@stu.ipbeja.pt>
2023-04-20 16:32:51 +0100

    Login and Register Update
    Did some changes on the layouts.
    Started to make the methods for the user registration and login.

--- app/src/main/java/com/pac/imonline/activity/LoginActivity.java
+++ app/src/main/java/com/pac/imonline/activity/LoginActivity.java
@@ -2,7 +2,9 @@ package com.pac.imonline.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.pac.imonline.R;
 
@@ -12,5 +14,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void openRegisterPage (View view){
+
+        Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(i);
+
     }
 }
--- app/src/main/java/com/pac/imonline/activity/RegisterActivity.java
+++ app/src/main/java/com/pac/imonline/activity/RegisterActivity.java
@@ -3,14 +3,95 @@ package com.pac.imonline.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.pac.imonline.R;
+import com.pac.imonline.activity.model.User;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText userField, emailField, passwordField;
+    private Button registerButton;
+    private ProgressBar progressBar;
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initializeComponents();
+
+        // User Register
+        progressBar.setVisibility(View.GONE);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                progressBar.setVisibility(View.VISIBLE);
+                String userText = userField.getText().toString();
+                String emailText = emailField.getText().toString();
+                String passwordText = passwordField.getText().toString();
+
+                if( !userText.isEmpty()) {
+                    if( !emailText.isEmpty()) {
+                        if( !passwordText.isEmpty()) {
+
+                            user = new User();
+                            user.setName(userText);
+                            user.setEmail(emailText);
+                            user.setPassword(passwordText);
+
+                            registerUser(user);
+
+                }else{
+                            Toast.makeText(RegisterActivity.this,
+                                    "Insert your password",
+                                    Toast.LENGTH_SHORT).show();
+
+                }
+
+                }else{
+
+                        Toast.makeText(RegisterActivity.this,
+                                "Insert your email",
+                                Toast.LENGTH_SHORT).show();
+
+                }
+
+                }else{
+
+                    Toast.makeText(RegisterActivity.this,
+                            "Insert your username",
+                            Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
     }
+
+    // method to register a user and make validations as we do it
+    public void registerUser(User user){
+
+
+
+    }
+
+    public void initializeComponents(){
+
+        userField = findViewById(R.id.username_register);
+        emailField = findViewById(R.id.email_register);
+        passwordField = findViewById(R.id.password_register);
+        registerButton = findViewById(R.id.register_button);
+        progressBar = findViewById(R.id.progress_bar_register);
+
+    }
+
 }
