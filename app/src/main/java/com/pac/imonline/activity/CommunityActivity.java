a9254e7de6f6f9de848cf42088c8be54c2e5ef96
RMXX <networkrmxx@gmail.com>
2023-05-15 11:54:58 +0100

    Communities page create
    Create the page

---
+++ app/src/main/java/com/pac/imonline/activity/CommunityActivity.java
@@ -0,0 +1,16 @@
+package com.pac.imonline.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pac.imonline.R;
+
+public class CommunityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_community);
+    }
+}
