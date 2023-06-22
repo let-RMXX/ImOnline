c34952bb31bce76a3f3e2e28af284864a124d5aa
Carlos Álvaro <c.alvaro@outlook.pt>
2023-06-22 20:40:15 +0100

    ADD/Edit education and Work
    Created class EditEducation/EditWork
    insert/update dos dados para a recyclerView das respectivas.

---
+++ app/src/main/java/com/pac/imonline/activity/EditEducationActivity.java
@@ -0,0 +1,54 @@
+package com.pac.imonline.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.pac.imonline.R;
+
+public class EditEducationActivity extends AppCompatActivity {
+
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, EditEducationActivity.class);
+        context.startActivity(intent);
+    }
+
+    private EditText editTextUni_name;
+    private EditText editTextDate;
+    private EditText editTextGraduation;
+    private EditText editTextDescription;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.edit_education);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+
+        this.editTextUni_name = findViewById(R.id.editTextUni_name);
+        this.editTextDate = findViewById(R.id.editTextDate);
+        this.editTextGraduation = findViewById(R.id.editTextGraduation);
+        this.editTextDescription = findViewById(R.id.editTextDescription);
+    }
+
+    public void editEducation(View view){
+        String uniName = this.editTextUni_name.getText().toString();
+        String date = this.editTextDate.getText().toString();
+        String graduation = this.editTextGraduation.getText().toString();
+        String description = this.editTextDescription.getText().toString();
+
+        EducationActivity newEducation = new EducationActivity(uniName, date, graduation, description);
+
+
+        //TODO APP DATAABSE PARA DAR INSERT OU UPDATE????
+        // AppDatabase.getInstance(this).getEducationDAO().insert(newEducationActivity);???
+        //finish();
+    }
+}
