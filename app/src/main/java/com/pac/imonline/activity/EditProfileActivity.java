d2cacc0e0bcddc42c64fbef07bddc86d751ff943
Carlos Álvaro <c.alvaro@outlook.pt>
2023-06-26 20:51:10 +0100

    Profile updates
    Profile Adapter e EditProfile Class

--- app/src/main/java/com/pac/imonline/activity/EditProfileActivity.java
+++ app/src/main/java/com/pac/imonline/activity/EditProfileActivity.java
@@ -2,15 +2,49 @@ package com.pac.imonline.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.pac.imonline.R;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, EditProfileActivity.class);
+        context.startActivity(intent);
+    }
+
+    TextInputEditText textInputEditTextName;
+    TextInputEditText textInputEditTextAbout;
+    TextInputEditText textInputEditTextPhone;
+    TextInputEditText textInputEditTextEmail;
+    TextInputEditText textInputEditTextLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editprofile);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        this.textInputEditTextName = findViewById(R.id.textInputEditTextName);
+        this.textInputEditTextAbout = findViewById(R.id.textInputEditTextAbout);
+        this.textInputEditTextPhone = findViewById(R.id.textInputEditTextPhone);
+        this.textInputEditTextEmail = findViewById(R.id.textInputEditTextEmail);
+        this.textInputEditTextLocation = findViewById(R.id.textInputEditTextLocation);
+    }
+
+    public void editProfile(View view){
+        String name = this.textInputEditTextName.getText().toString();
+        String about = this.textInputEditTextAbout.getText().toString();
+        String phone = this.textInputEditTextPhone.getText().toString();
+        String email = this.textInputEditTextEmail.getText().toString();
+        String location = this.textInputEditTextLocation.getText().toString();
+
+        //ProfileActivity newProfile = new ProfileActivity(0,name, about, phone, "", location);
     }
 }
