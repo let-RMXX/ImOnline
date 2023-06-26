62ece574cf889e2abf51cf89a78e22cf4153fe2c
Ana Lopes <24286@stu.ipbeja.pt>
2023-06-26 12:58:23 +0100

    Toolbar Main

--- app/src/main/java/com/pac/imonline/activity/activity/MainActivity.java
+++ app/src/main/java/com/pac/imonline/activity/activity/MainActivity.java
@@ -10,13 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.pac.imonline.R;
-import com.pac.imonline.activity.activity.DatabaseAuth;
-import com.pac.imonline.activity.activity.LoginActivity;
-import com.pac.imonline.helper.DatabaseConfig;
 
 public class MainActivity extends AppCompatActivity {
-
-    private DatabaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Toolbar Config
         Toolbar toolbar = findViewById(R.id.toolbarMain);
-        toolbar.setTitle("Im Online");
         setSupportActionBar(toolbar);
-
-        //object config
-        auth = DatabaseConfig.getDatabaseAuth();
 
     }
 
@@ -42,36 +33,4 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
 
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()){
-
-            case R.id.menuSignOut:
-                signOutUser();
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-
-                break;
-
-        }
-
-        return super.onOptionsItemSelected(item);
-
-    }
-
-    private void signOutUser(){
-
-        try {
-
-            auth.signOut();
-
-        }catch (Exception e){
-
-            e.printStackTrace();
-
-        }
-
-    }
-
 }
--- app/src/main/java/com/pac/imonline/activity/activity/RegisterActivity.java
+++ app/src/main/java/com/pac/imonline/activity/activity/RegisterActivity.java
@@ -1,136 +1,19 @@
 package com.pac.imonline.activity.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.pac.imonline.R;
-import com.pac.imonline.activity.activity.DatabaseAuth;
-import com.pac.imonline.activity.activity.MainActivity;
-import com.pac.imonline.helper.DatabaseConfig;
-import com.pac.imonline.model.User;
 
 
 public class RegisterActivity extends AppCompatActivity {
-
-    private EditText userField, emailField, passwordField;
-    private Button signInButton;
-    private User user;
-
-    private DatabaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        inicializeComponents();
-
-        // Sign In User
-        signInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String userText = userField.getText().toString();
-                String emailText = emailField.getText().toString();
-                String passwordText = passwordField.getText().toString();
-
-                if ( !userText.isEmpty()){
-
-                    if ( !emailText.isEmpty()){
-
-                        if ( !passwordText.isEmpty()){
-
-                            user = new User();
-                            user.setUsername(userText);
-                            user.setEmail(emailText);
-                            user.setPassword(passwordText);
-                            Register(user);
-
-                        }else {
-
-                            Toast.makeText(RegisterActivity.this,
-                                    "Password Missing!",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-
-                    }else {
-
-                        Toast.makeText(RegisterActivity.this,
-                                "Email Missing!",
-                                Toast.LENGTH_SHORT).show();
-
-                    }
-
-                }else {
-
-                    Toast.makeText(RegisterActivity.this,
-                            "Username Missing!",
-                            Toast.LENGTH_SHORT).show();
-
-                }
-
-            }
-        });
 
     }
-
-    // Method responsible for registering users with email and password and
-    // make validations when making the registration
-    public void Register(User user){
-
-        auth = DatabaseConfig.getDatabaseAuth();
-        auth.createUserWithEmailAndPassword(
-
-                user.getEmail(),
-                user.getPassword()
-
-        ).addOnCompleteListener(
-
-                this,
-                new OnCompleteListener<AuthResult>(){
-
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task){
-
-                        if (task.isSucessful()){
-
-                            Toast.makeText(RegisterActivity.this,
-                                    "Successfully Registered",
-                                    Toast.LENGTH_SHORT).show();
-
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                            finish();
-
-                        }else{
-
-
-
-                        }
-
-                    }
-
-                }
-
-    }
-        );
-
-}
-
-    public void inicializeComponents(){
-
-        userField = findViewById(R.id.enterUsernameRegister);
-        emailField = findViewById(R.id.enterEmailRegister);
-        passwordField = findViewById(R.id.enterPasswordRegister);
-        signInButton = findViewById(R.id.SignUpButton);
-
-    }
-
 }
