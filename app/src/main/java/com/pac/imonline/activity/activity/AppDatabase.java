8333f5e8e949af8a680453b794caa224270e8890
RMXX <networkrmxx@gmail.com>
2023-06-28 20:09:49 +0100

    TESTING IGNORE

--- app/src/main/java/com/pac/imonline/activity/activity/AppDatabase.java
+++ app/src/main/java/com/pac/imonline/activity/activity/AppDatabase.java
@@ -6,11 +6,12 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Community.class, UserEntity.class}, version = 2, exportSchema = false)
+@Database(entities = {Community.class, UserEntity.class, EducationActivity.class, ProfileActivity.class, WorkActivity.class}, version = 2, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
 
     private static final String dbName = "imonlinedb";
     private static AppDatabase appDatabase;
+    private static AppDatabase INSTANCE;
 
     public static synchronized AppDatabase getAppDatabase(Context context){
         if (appDatabase == null) {
@@ -21,7 +22,31 @@ public abstract class AppDatabase extends RoomDatabase {
         return appDatabase;
     }
 
+    public static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "imonlinedb").allowMainThreadQueries()
+                    .addCallback(new Callback() {
+                        @Override
+                        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                            super.onCreate(db);
+                            db.execSQL("INSERT INTO profileActivity VALUES(1,'','name','about', 'phone', 'email', 'location')");
+                        }
+
+                    })
+                    .build();
+        }
+
+        return INSTANCE;
+    }
+
     public abstract UserDao userDao();
 
     public abstract CommunityDao getCommunityDao();
+
+    public abstract ProfileDAO getProfileDAO();
+
+    public abstract EducationDAO getEducationDAO();
+
+    public abstract WorkDAO getWorkDAO();
 }
